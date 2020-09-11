3b9f31094e34a9b7e7bf75a19ef0f6cf1a0d6712
Joshua Whetton <whetton.josh@gmail.com>
2020-09-11 16:18:55 -0400

    Food monitor will equip extra food from your bank.
    Upon exhausting equipped food, we'll equip food from the bank if available. All food, both bank and equipped, is exhausted, we'll drop out of combat.

--- melvor.js
+++ melvor.js
@@ -13,6 +13,7 @@ class MelvorDriver {
     foodList = []
     isAutoLooting = false
     isTrackingFood = false
+    isOutOfFood = () => equippedFood[0].itemID === equippedFood[1].itemID && equippedFood[0].itemID === equippedFood[2].itemID
     getFoodCount = () => equippedFood[currentCombatFood].qty
     logger = (s) => {
         console.log(`[${new Date()}] - ${s}`)
@@ -42,6 +43,19 @@ class MelvorDriver {
         }
         this.logger("Nothing to do: no auto-looter running.")
     }
+    findFood = () => {
+        for (i = 0; i < bank.length; i++) {
+            thisItem = items[bank[i].id]
+            // build a list of food in my bank
+            if (thisItem.canEat) {
+                f = new MyFood()
+                f.id = thisItem.id
+                f.name = thisItem.name
+                f.bankLocation = i
+                this.foodList.push(f)
+            }
+        }
+    }
     startWatchingFood = (t) => {
         if (t == null) {
             this.logger("Usage: startWatchingFood(t) where 't' is interval in ms.")
@@ -53,20 +67,29 @@ class MelvorDriver {
         }
         // Start monitoring food every t ms.
         this.isTrackingFood = true
+        this.findFood() // find any eligible food in our bank
         this.foodTracker = setInterval(() => {
-            if (this.getFoodCount() <= 10) {
-                if (currentCombatFood === 2) { // we're completely out of food; exposed by Melvor.
-                    stopCombat(false, true, true) // death, stopDungeon, runAway
-                    alert("Out of food! Combat ended.")
-                    this.logger("Stopped combat due to lack of food.")
-                    this.isTrackingFood = false
-                    clearInterval(this.foodTracker)
-                } else {
-                    selectEquippedFood(currentCombatFood+1) // select my next slot of food.
-                    this.logger("Swapped food!")
-                    // TODO: Add something about adding any bank food.
-                    // equipFood(f.BANKID, f.id, f.MAX)
+            if (this.getFoodCount() > 0) return // we have equipped food to eat
+            if (this.isOutOfFood() && this.foodList.length === 0) { // completely out of food
+                stopCombat(false, true, true) // death, stopDungeon, runAway
+                alert("Out of food! Combat ended.")
+                this.logger("Stopped combat due to lack of food.")
+                this.isTrackingFood = false
+                clearInterval(this.foodTracker)
+            } else { // we have food somewhere but need to equip it
+                for (var i=0; i++; i < equippedFood.length) {
+                    console.log("We are looking for food")
+                    if (equippedFood[i].qty > 0) {
+                        selectEquippedFood(equippedFood[i])
+                        this.logger(`Swapped to food in slot ${i}!`)
+                        return
+                    }
                 }
+                // We don't have any additional food equipped to swap
+                // to if we're here, but we must have something in the bank.
+                f = this.foodList.pop()
+                equipFood(f.BANKID, f.id, f.MAX)
+                this.logger(`Equipped ${f.name} from the bank since we didn't have food equipped.`)
             }
         }, t)
         this.logger("Started watching your food.")
@@ -82,18 +105,5 @@ class MelvorDriver {
     }
     status = () => {
         console.log(`Auto-looting: ${this.isAutoLooting}, Food tracking: ${this.isTrackingFood}, Bank has ${this.foodList.length} eligible food items.`)
-    }
-    findFood = () => {
-        for (i = 0; i < bank.length; i++) {
-            thisItem = items[bank[i].id]
-            // build a list of food in my bank
-            if (thisItem.canEat) {
-                f = new MyFood()
-                f.id = thisItem.id
-                f.name = thisItem.name
-                f.bankLocation = i
-                this.foodList.push(f)
-            }
-        }
     }
 }
