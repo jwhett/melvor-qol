ac91567cf3a7bc897e6b5e6c4b60c60921c1b3cb
Josh Whetton <jwhett@protonmail.com>
2020-09-12 17:01:33 -0400

    Able to harvest and plant allotments.

--- farming.js
+++ farming.js
@@ -1,5 +1,11 @@
-newFarmingAreas // list that holds the area objects
-                // id, areaName, patches[]
+function patchHandler(areaID, patchId, patch) {
+    if (patch.hasGrown || patch.unlocked) {
+        harvestSeed(areaId, patchId)
+    }
+    selectedPatch = [areaId, patchId]
+    selectedSeed = something
+    plantSeed()
+}
 
 function loadSeedBag() {
     var a = []
@@ -10,15 +16,40 @@ function loadSeedBag() {
     return a
 }
 
-function patchHandler(areaID, patchId, patch) {
-    if (patch.hasGrown || patch.unlocked) {
-        harvestSeed(areaId, patchId)
-    }
-    selectedPatch = [areaId, patchId]
-    selectedSeed = something
-    plantSeed()
-}
+var mySeeds = loadSeedBag()
 
-selectedPatch = [0, 0]
-selectedSeed = 146
-plantSeed()
+for (var i=0; i<newFarmingAreas[0].patches.length; i++) {
+    var patch = newFarmingAreas[0].patches[i]
+    var selectedPatch = [0,i]
+    selectedSeed = 0
+    if (!patch.unlocked) {
+        console.log("Patch is not unlocked.")
+        continue
+    }
+    if (!patch.hasGrown && patch.seedID !== 0) {
+        console.log("Patch isn't ready for harvest.")
+        continue
+    }
+    if (!patch.hasGrown && patch.seedID === 0) {
+        // empty patch, need to pick a fresh seed
+        mySeeds.forEach(seed => {
+            if (seed.tier === "Allotment") {
+                selectedSeed = seed.id+1
+            }
+        })
+    } else {
+        // not empty
+        if (checkBankForItem(patch.seedID)) {
+            selectedSeed = patch.seedID
+        } else {
+            mySeeds.forEach(seed => {
+                if (seed.tier === "Allotment") {
+                    selectedSeed = seed.id+1
+                }
+            })
+        }
+        harvestSeed(0, i)
+    }
+    plantSeed()
+    console.log("Seed planted")
+}
