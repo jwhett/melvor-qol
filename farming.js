cf678c436f516747fc54177b57039ba547fddca6
Joshua Whetton <whetton.josh@gmail.com>
2020-09-13 12:51:10 -0400

    remove all the debug logging from farming

--- farming.js
+++ farming.js
@@ -1,3 +1,4 @@
+// Helpers
 function getSeeds() {
     var a = []
     for (i = 0; i < items.length; i++) {
@@ -25,63 +26,40 @@ function getNextSeedIDByTier(seeds, t) {
         } catch (err) {
             console.error(`getNextSeedIDByTier() error: ${err}`);
         }
-        if (seedFound !== undefined) {
-            if (checkBankForItem(seedFound)) {
-                console.log(`{inBank: true, seedFound: ${seedFound}}`);
-                break
-            } else {
-                console.log(`{inBank: false, seedFound: ${seedFound}}`);
-                continue
-            }
-        }
+        if (seedFound !== undefined && checkBankForItem(seedFound)) break
     }
     return seedFound
 }
+// Main
 function reapAndSow() {
-    console.log("> Reaping...")
     let realSeeds = getSeeds()
     for (let locationID=0; locationID < newFarmingAreas.length; locationID++) {
-        console.log(`>> {locationID: ${locationID}, areaName: ${newFarmingAreas[locationID].areaName}}`)
-      for (let patchID=0; patchID < newFarmingAreas[locationID].patches.length; patchID++) {
-          console.log(`>>> {patchID: ${patchID}}`)
-          let patch = newFarmingAreas[locationID].patches[patchID]
-          let areaName = newFarmingAreas[locationID].areaName
-          selectedPatch = [newFarmingAreas[locationID].id, patchID]
-          selectedSeed = 0
-          console.log(`>>> {patch: ${patch}, selectedPatch: ${selectedPatch}, areaName: ${areaName}, selectedSeed: ${selectedSeed}}`)
+        for (let patchID=0; patchID < newFarmingAreas[locationID].patches.length; patchID++) {
+            let patch = newFarmingAreas[locationID].patches[patchID]
+            let areaName = newFarmingAreas[locationID].areaName
+            selectedPatch = [newFarmingAreas[locationID].id, patchID]
+            selectedSeed = 0
 
-          // can we plant?
-          if (!patch.unlocked) {console.log(">>> not unlocked");continue}
-          if (!patchIsReady(patch)) {console.log(">>> not ready");continue}
+            // can we plant?
+            if (!patch.unlocked) continue
+            if (!patchIsReady(patch)) continue
 
-          // what seed do i plant?
-          if (patchIsEmpty(patch)) {
-              // no harvest in this block
-              selectedSeed = getNextSeedIDByTier(realSeeds, areaName)
-              console.log(`>>> empty, decided on this seed: {selectedSeed: ${selectedSeed}}`)
-          } else {
-              // try to plant the same seed
-              // we'll need to harvest before leaving block
-              if (checkBankForItem(patch.seedID)) {
-                  selectedSeed = patch.seedID
-                  console.log(`>>> not empty, decided on this seed: {selectedSeed: ${selectedSeed}}`)
-              } else {
+            // what seed do i plant?
+            if (patchIsEmpty(patch)) {
+            // no harvest in this block
+                selectedSeed = getNextSeedIDByTier(realSeeds, areaName)
+            } else {
+                // try to plant the same seed
+                // we'll need to harvest before leaving block
+                if (checkBankForItem(patch.seedID)) {
+                    selectedSeed = patch.seedID
+                } else {
                   // don't have the same seed to plant
                   selectedSeed = getNextSeedIDByTier(realSeeds, areaName)
-                  console.log(`>>> not empty & doin't have same, decided on this seed: {selectedSeed: ${selectedSeed}}`)
-              }
-              console.log(`>>> harvesting: {locationID: ${locationID}, patchID: ${patchID}}`)
-              harvestSeed(locationID, patchID)
-          }
-          console.log(`>>> planting: {selectedSeed: ${selectedSeed}, selectedPatch: ${selectedPatch}}`)
-          plantSeed()
-          if (patchIsEmpty(patch)) {
-              console.log(">>> planting FAILED <<<")
-          } else {
-              console.log(`>>> Seed ${selectedSeed} planted in ${areaName}`)
-          }
-      }
-      console.log(`>> done with: {locationID: ${locationID}, areaName: ${newFarmingAreas[locationID].areaName}`)
+                }
+                harvestSeed(locationID, patchID)
+            }
+            plantSeed()
+        }
     }
-    console.log("> done with all locations");
 }
