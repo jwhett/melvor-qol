4ebd73dc7360e310b5a460bc49af2071fb23a711
Joshua Whetton <whetton.josh@gmail.com>
2020-09-13 13:01:15 -0400

    sell all harvested items in bank.
    pairs well with the auto-harvester

--- inventory.js
+++ inventory.js
@@ -1,4 +1,6 @@
 function allOfTypeInBank(t) {
+    // Returns a list of items with type t
+    // that are NOT locked.
     let things = []
     for (let i=0; i < bank.length; i++) {
         if (bank[i].locked) continue
@@ -17,5 +19,11 @@ function learnTokens() {
     tokens.forEach(token => {
         claimBankToken(0,token.id)
     })
+}
 
+function sellHarvest() {
+    let crops = allOfTypeInBank("Havest") // not a typo...
+    crops.forEach(crop => {
+        sellItem(crop.id)
+    })
 }
