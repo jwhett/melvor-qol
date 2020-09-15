d85fbcba6968a4c452d0ce9510c5faee62df99a0
Josh Whetton <jwhett@protonmail.com>
2020-09-14 22:24:41 -0400

    Decrease interval so the fires overlap during cooking.

--- 99-monitors.js
+++ 99-monitors.js
@@ -6,5 +6,5 @@ watchers.Farming = new Watcher(reapAndSow, 120000, "Farming")
 watchers.Food = new Watcher(foodTracker, 10000, "Food")
 watchers.Looting = new Watcher(lootAll, 30000, "Looting")
 watchers.Token = new Watcher(learnTokens, 30000, "Tokens")
-watchers.Fire = new Watcher(lightCookingFire, fireIntervalMin-10, "Fire")
+watchers.Fire = new Watcher(lightCookingFire, fireIntervalMin-5000, "Fire")
 for (watcher in watchers) {watchers[watcher].start()}
