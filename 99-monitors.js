c6f70c89c62a10261718cc43ad8e958f2d449166
Josh Whetton <josh@whetton.net>
2020-09-20 12:54:41 -0400

    Reorganize monitors

--- 99-monitors.js
+++ 99-monitors.js
@@ -3,16 +3,17 @@ const watchers = {};
 for (t of ['Junk', 'Gem', 'Havest', 'Logs']) {
     watchers[t] = new Watcher(sellAllOfType, 120000, t, t);
 }
-watchers.Bones = new Watcher(buryBones, 15000, 'Bones');
-watchers.Food = new Watcher(foodTracker, 10000, 'Food');
-watchers.Looting = new Watcher(lootAll, 15000, 'Looting');
-watchers.Token = new Watcher(learnTokens, 15000, 'Tokens');
-watchers.Fire = new Watcher(lightCookingFire, 250, 'Fire');
-watchers.Eating = new Watcher(combatAutoEat, 1500, 'Eating');
-watchers.Farming = new Watcher(reapAndSow, 60000, 'Farming');
-watchers.Save = new Watcher(saveAndDownload, 1600000, 'Game Saving');
-watchers.Burnt = new Watcher(sellAllOfNameSubstring, 5000, 'Burnt Food', 'Burnt');
-watchers.Feathers = new Watcher(sellAllOfNameSubstring, 5000, 'Feathers', 'Feathers');
+watchers.Bones = new Watcher(buryBones, 15000, 'Bury bones');
+watchers.Sort = new Watcher(sortBank, 60000, "Bank Sorting");
+watchers.Looting = new Watcher(lootAll, 15000, 'Auto-looting');
+watchers.Token = new Watcher(learnTokens, 15000, 'Learn tokens');
+watchers.Farming = new Watcher(reapAndSow, 60000, 'Auto-farming');
+watchers.Fire = new Watcher(lightCookingFire, 250, 'Cooking fire');
+watchers.Eating = new Watcher(combatAutoEat, 1500, 'Combat eating');
+watchers.Food = new Watcher(foodTracker, 10000, 'Combat food swap');
+watchers.Save = new Watcher(saveAndDownload, 1600000, 'Game saving');
+watchers.Burnt = new Watcher(sellAllOfNameSubstring, 5000, 'Burnt food seller', 'Burnt');
+watchers.Feathers = new Watcher(sellAllOfNameSubstring, 5000, 'Feather seller', 'Feathers');
 // for (watcher in watchers) {watchers[watcher].start()}
 
 function showWatchers() {
