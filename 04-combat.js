0020411e1ae9bb17874bc64a3677919083fce885
Josh Whetton <josh@whetton.net>
2020-11-22 11:56:05 -0500

    Add arbitrary HP cut-off for combatAutoEat().
    You can now set at which HP break point you
    would like to eat. Default behavior of eating
    when you're missing HP greater than or equal
    to your currently equppied food's healing potential.

--- 04-combat.js
+++ 04-combat.js
@@ -1,11 +1,21 @@
 /* eslint-disable no-unused-vars, no-undef */
-function combatAutoEat() {
-    const missingHP = maxHitpoints - combatData.player.hitpoints;
-    if (equippedFood[currentCombatFood].qty > 0 && combatData.player.hitpoints < maxHitpoints) {
-        if (missingHP >= items[equippedFood[currentCombatFood].itemID].healsFor * 10) {
-            eatFood();
-        }
+function combatAutoEat(healWhenMissing = null) {
+    if (!isInCombat) {
+        return;
     }
+    const missingHP = maxHitpoints - combatData.player.hitpoints;
+    if (healWhenMissing === null) {
+        // if i have food and am missing some amount of HP
+        if (equippedFood[currentCombatFood].qty > 0 && combatData.player.hitpoints < maxHitpoints) {
+            // if i'm missing enough HP for my food to heal
+            if (missingHP >= items[equippedFood[currentCombatFood].itemID].healsFor * 10) {
+                eatFood(efficiency = 100, autoEat = true);
+            }
+        }
+    } else if (missingHP >= healWhenMissing) {
+            eatFood(efficiency = 100, autoEat = true);
+    }
+    // loot and exit combat if i'm out of food now
     if (equippedFood[currentCombatFood].qty === 0) {
         lootAll();
         stopCombat();
